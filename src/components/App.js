d4844738ccafe15c94a581609282ed95f8e7b33d
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-02-10 00:44:10 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [greet, setGreet] = useState("");
+  const greetFnc = (e) => {
+    setGreet(`Hello ${e.target.value}!`);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <label>Enter your name:</label>
+      <input onChange={greetFnc} type="text" />
+      <p>{greet}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
